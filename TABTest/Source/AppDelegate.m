074527d57d4f8b8d0a4a9c9d55f88e6454f5bf89
Wyszo <wyszo@Tomeks-MacBook-Pro.local>
2013-10-19 17:36:59 +0200

    Project structure cleanup

---
+++ TABTest/Source/AppDelegate.m
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.m
+//  TABTest
+//
+//  Created by Wyszo on 10/19/13.
+//  Copyright (c) 2013 Synappse. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+#import "ViewController.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+    self.window.rootViewController = self.viewController;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+}
+
+@end
